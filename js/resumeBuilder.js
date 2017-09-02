83264f5cabc9c4c84415764a7e75ef74b102d86b
Mohit Menghnani <m.menghnani@gmail.com>
2017-09-01 18:16:41 -0700

    started online courses

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -27,7 +27,6 @@ var formattedEmail = HTMLemail.replace("%data%",bio.contacts.email);
 $("#topContacts").append(formattedEmail);
 $("#footerContacts").append(formattedEmail);
 
-
 var formattedTwitter = HTMLtwitter.replace("%data%",bio.contacts.twitter);
 $("#topContacts").append(formattedTwitter);
 $("#footerContacts").append(formattedTwitter);
@@ -45,63 +44,58 @@ $("#header").append(formattedBioPic);
 
 var formattedMsg = HTMLwelcomeMsg.replace("%data%",bio.welcomeMessage);
 $("#header").append(formattedMsg);
-
 $("#header").append(HTMLskillsStart);
 
-for(var i = 0;i < 3; i++)
+for(var i = 0;i < 3; i++) //Print skills
 {
-var formattedSkills = HTMLskills.replace("%data%",bio.skills[i]);
-$("#header").append(formattedSkills);
+    var formattedSkills = HTMLskills.replace("%data%",bio.skills[i]);
+    $("#header").append(formattedSkills);
 }
 
  var education = {
            "schools" : [{"name":"University of Florida","location":"Gainesville","degree":"Masters in Computer Science","dates":"August 2010 to May 2012"},
                          {"name":"IIIT Allahabad","location":"Allahabad","degree":"B.Tech(IT)","dates":"August 2006 to May 2010"}],
-           "onlineCourses" : [{"title":"Java Programming","school":"U of Michigan","dates":"July 2014 to July 2015"},{"title":"FrontEnd Nanodegree","school":"Udacity","dates":"July 2017 to December 2017"}]
-           //display: function
+           "onlineCourses" : [{"title":"Java Programming","school":"U of Michigan","dates":"July 2014 to July 2015"},
+           {"title":"FrontEnd Nanodegree","school":"Udacity","dates":"July 2017 to December 2017"}]
+           //"display": true
        };
 
-var formattedSchoolName = HTMLschoolName.replace("%data%",education.schools[0].name);
-$("#education").append(formattedSchoolName);
+$("#main").append(HTMLschoolStart);
 
-var work = {[{
+for(var i = 0; i<2; i++){
+    var formattedSchoolName = HTMLschoolName.replace("%data%",education.schools[i].name);
+    $(".education-entry").append(formattedSchoolName);
+
+    var formattedSchoolLocation = HTMLschoolLocation.replace("%data%",education.schools[i].location);
+    $(".education-entry").append(formattedSchoolLocation);
+
+    var formattedSchoolDegree = HTMLschoolDegree.replace("%data%",education.schools[i].degree)
+    $(".education-entry").append(formattedSchoolDegree);
+
+    var formattedSchoolDates = HTMLschoolDates.replace("%data%",education.schools[i].dates)
+    $(".education-entry").append(formattedSchoolDates);
+}
+
+$(".education-entry").append(HTMLonlineClasses);
+
+for(var i=0; i<2; i++){
+
+    var formattedOnlineTitle = HTMLonlineTitle.replace("%data%",education.onlineCourses[i].title);
+    $(".education-entry").append(formattedOnlineTitle);
+
+    var formattedOnlineSchool = HTMLonlineSchool.replace("%data%".education.onlineCourses[i].school);
+    HTMLonlineDates
+    HTMLonlineURL
+}
+
+/*var work = {[{
     "jobs" : [{"employer" : "AppDirect", "title" : "Senior Engineer", "dates" : "Feb 2015 to present","location" : "sunnyvale","description" : "asdsadasd asdsad asdsdsa sadsadasd"},
     { "employer" : "Citrix", "title" : "Engineer", "dates" : "July 2012 to Feb 2015", "location" : "Florida","description" : "asdsadasd asdsad asdsdsa sadsadasd"},
     {"employer" : "Sumtotal","title" : "Junior Engineer","dates" : "July 2011 to June 2012","location" : "NY","description" : "asdsadasd asdsad asdsdsa sadsadasd"}],
-    "display" : "true"};
+    "display" : "true"};*/
 
 
 
-/*var bio = {
-//age : 28,
-degree : "masters in computer engineering",
-interests : "front end",
-skills : ["programming","swimming"]
-};
-
-$("#header").append(bio.age);
-$("#header").append(bio.degree);
-$("#header").append(bio.interests);
-
-
-*/
-
-//$.append(HTMLworkStart);
-var formattedWorkStart = "January 2010 to present";
-
-var formattedEmployer = HTMLworkEmployer.replace("%data%","AppDirect");
-//$("#workExperience").append(formattedEmployer);
-$(formattedEmployer).insertAfter("#workExperience");
-var formattedWorkTitle = HTMLworkTitle.replace("%data%","Senior Engineer");
-
-//var  = ' - %data%</a>';
-
-var HTMLworkDates = '<div class="date-text">%data%</div>';
-
-var HTMLworkLocation = '<div class="location-text">%data%</div>';
-
-var formattedWorkDescription = HTMLworkDescription.replace("%data","asdasdsad askdjhsadjh kjashdjkashdj ksahdk sd")
-$(formattedEmployer).insertAfter("workExperience");
 
 
 
