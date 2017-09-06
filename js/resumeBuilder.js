4849f10045734caff71d72bf8cf5366899f709b4
Mohit Menghnani <m.menghnani@gmail.com>
2017-09-05 19:18:48 -0700

    push a beautiful version

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -46,23 +46,47 @@ var formattedMsg = HTMLwelcomeMsg.replace("%data%",bio.welcomeMessage);
 $("#header").append(formattedMsg);
 $("#header").append(HTMLskillsStart);
 
-for(var i = 0;i < 3; i++) //Print skills
+//Print skills
+var i = 0;
+while(bio.skills[i])
 {
     var formattedSkills = HTMLskills.replace("%data%",bio.skills[i]);
     $("#header").append(formattedSkills);
+    i++;
 }
 
 
 var work = {
-    "jobs" : [{"employer" : "AppDirect", "title" : "Senior Engineer", "dates" : "Feb 2015 to present","location" : "Sunnyvale, CA","description" : "asdsadasd asdsad asdsdsa sadsadasd"},
-    { "employer" : "Citrix", "title" : "Engineer", "dates" : "July 2012 to Feb 2015", "location" : "Fort Lauderdale,FL","description" : "asdsadasd asdsad asdsdsa sadsadasd"},
-    {"employer" : "Sumtotal","title" : "Junior Engineer","dates" : "July 2011 to June 2012","location" : "Syracuse, NY","description" : "asdsadasd asdsad asdsdsa sadsadasd"}]
-  //  "display" : "true"
-    };
-
+    "jobs": [{
+            "employer": "AppDirect",
+            "title": "Senior Engineer",
+            "dates": "Feb 2015 to present",
+            "location": "Sunnyvale, CA",
+            "description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis ac ornare quam. Nunc eros nunc, convallis tempor eros at, facilisis finibus magna. Curabitur finibus elementum leo, in vulputate libero efficitur ut. Ut in lacus metus. Aliquam vitae justo id est aliquam vehicula. Nam rhoncus ornare nisl in dapibus. Vivamus eget commodo quam. Ut non turpis ante. Cras dictum ex et risus scelerisque tempus. Ut dapibus eros sit amet mi commodo, id tristique nulla dignissim."
+        },
+        {
+            "employer": "Citrix",
+            "title": "Engineer",
+            "dates": "July 2012 to Feb 2015",
+            "location": "Fort Lauderdale,FL",
+            "description": "Donec in nisl eu mi interdum aliquam. Suspendisse justo tellus, lobortis eget nulla vel, varius dapibus nisi. Maecenas imperdiet elit molestie nulla luctus efficitur. Proin id quam eget enim tempus lobortis. Nulla faucibus, diam quis ultricies lacinia, sapien odio malesuada lorem, ut mattis magna odio nec justo. "
+        },
+        {
+            "employer": "Sumtotal",
+            "title": "Junior Engineer",
+            "dates": "July 2011 to June 2012",
+            "location": "Syracuse, NY",
+            "description": "Nullam sed est finibus, tincidunt nibh id, molestie magna. Etiam tempus fermentum nulla non tincidunt. Aenean iaculis eros placerat nulla ultricies, ut dapibus tellus imperdiet. Mauris sed feugiat diam. Nullam nulla arcu, fermentum at rutrum nec, ultricies sed orci. Cras sed ligula eros."
+        }
+    ]
+    //  "display" : "true"
+};
 $("#workExperience").append(HTMLworkStart);
 
-for(var i = 0; i<3 ;i ++){
+//for(var i = 0; i<3 ;i ++){
+var i = 0;
+while(work.jobs[i])
+{
     var formattedworkEmployer = HTMLworkEmployer.replace("%data%",work.jobs[i].employer);
     $(".work-entry").append(formattedworkEmployer);
 
@@ -77,81 +101,131 @@ for(var i = 0; i<3 ;i ++){
 
     var formattedworkDescription = HTMLworkDescription.replace("%data%",work.jobs[i].description);
     $(".work-entry").append(formattedworkDescription); //Work
+    i++;
 }
 
 var projects = {
-
-    "project" : [
-                {"title" : "Library Management system","dates" : "July 2012 to Feb 2015", "description" : "asdasdasd asdasd adsasdad",
-                "images" : ["https://goo.gl/a2yj7s","https://goo.gl/a2yj7s","https://goo.gl/a2yj7s"]},
-                {"title" : "File Transactions in the cloud","dates" : "Feb 2015 to present", "description" : "asdasd asdasd asdasdasdasd",
-                "images" : ["https://goo.gl/a2yj7s","https://goo.gl/a2yj7s","https://goo.gl/a2yj7s","https://goo.gl/a2yj7s"]}]
-
+    "project": [{
+            "title": "Library Management System",
+            "dates": "Feb 2015",
+            "description": "asdasdasd asdasd adsasdad",
+            "images": ["https://goo.gl/a2yj7s", "https://goo.gl/a2yj7s", "https://goo.gl/a2yj7s"]
+        },
+        {
+            "title": "File Transactions in the Cloud",
+            "dates": "Present",
+            "description": "asdasd asdasd asdasdasdasd",
+            "images": ["https://goo.gl/a2yj7s", "https://goo.gl/a2yj7s", "https://goo.gl/a2yj7s", "https://goo.gl/a2yj7s"]
+        }
+    ]
 };
 
 $("#projects").append(HTMLprojectStart);
 
-for(var i = 0; i < 2; i++){
-
-var formattedProjectTitle = HTMLprojectTitle.replace("%data%",projects.project[i].title);
-    $(".project-entry").append(formattedProjectTitle);
-
-var formattedProjectDates = HTMLprojectDates.replace("%data%",projects.project[i].dates);
-    $(".project-entry").append(formattedProjectDates);
-
-var formattedProjectDescription = HTMLprojectDescription.replace("%data%",projects.project[i].description);
-    $(".project-entry").append(formattedProjectDescription);
-
-var j = 0;
-while(projects.project[i].images[j])
+//for(var i = 0; i < 2; i++){
+var i = 0;
+while(projects.project[i])
 {
-    var formattedProjectImages =  HTMLprojectImage.replace("%data%",projects.project[i].images[j]);
-    $(".project-entry").append(formattedProjectImages);
-    j++; }
-}
+  var formattedProjectTitle = HTMLprojectTitle.replace("%data%",projects.project[i].title);
+      $(".project-entry").append(formattedProjectTitle);
 
- var education = {
-           "schools" : [{"name":"University of Florida","location":"Gainesville","degree":"Masters in Computer Science","dates":"August 2010 to May 2012"},
-                         {"name":"IIIT Allahabad","location":"Allahabad","degree":"B.Tech(IT)","dates":"August 2006 to May 2010"}],
-           "onlineCourses" : [{"title":"Java Programming","school":"U of Michigan","dates":"July 2014 to July 2015","URL":"https://www.umichigan.edu"},
-           {"title":"FrontEnd Nanodegree","school":"Udacity","dates":"July 2017 to December 2017","URL":"https://www.udacity.com"}]
-           //"display": true
-       };
+  var formattedProjectDates = HTMLprojectDates.replace("%data%",projects.project[i].dates);
+      $(".project-entry").append(formattedProjectDates);
+
+  var formattedProjectDescription = HTMLprojectDescription.replace("%data%",projects.project[i].description);
+      $(".project-entry").append(formattedProjectDescription);
+
+  var j = 0;
+  while(projects.project[i].images[j])
+  {
+      var formattedProjectImages =  HTMLprojectImage.replace("%data%",projects.project[i].images[j]);
+      $(".project-entry").append(formattedProjectImages);
+      j++; }
+  i++;
+  }
+
+var education = {
+    "schools": [{
+            "name": "University of Florida",
+            "location": "Gainesville",
+            "degree": "Masters in Computer Science",
+            "majors": ["Computer Science", "Electronics"],
+            "dates": "May 2012"
+        },
+        {
+            "name": "IIIT Allahabad",
+            "location": "Allahabad",
+            "majors": ["Information Technology", "Management"],
+            "degree": "B.Tech(IT)",
+            "dates": "August 2006 to May 2010"
+        }
+    ],
+    "onlineCourses": [{
+            "title": "Java Programming",
+            "school": "University of Michigan",
+            "dates": "July 2015",
+            "URL": "https://www.umichigan.edu"
+        },
+        {
+            "title": "FrontEnd Nanodegree",
+            "school": "Udacity",
+            "dates": "December 2017",
+            "URL": "https://www.udacity.com"
+        }
+    ]
+    //"display": true
+};
 
 $("#education").append(HTMLschoolStart);
 
-for(var i = 0; i<2; i++){
+var i = 0;
+while(education.schools[i]){
     var formattedSchoolName = HTMLschoolName.replace("%data%",education.schools[i].name);
     $(".education-entry").append(formattedSchoolName);
+
+    var formattedSchoolDegree = HTMLschoolDegree.replace("%data%",education.schools[i].degree);
+    $(".education-entry").append(formattedSchoolDegree);
 
     var formattedSchoolLocation = HTMLschoolLocation.replace("%data%",education.schools[i].location);
     $(".education-entry").append(formattedSchoolLocation);
 
-    var formattedSchoolDegree = HTMLschoolDegree.replace("%data%",education.schools[i].degree)
-    $(".education-entry").append(formattedSchoolDegree);
 
-    var formattedSchoolDates = HTMLschoolDates.replace("%data%",education.schools[i].dates)
+    var formattedSchoolDates = HTMLschoolDates.replace("%data%",education.schools[i].dates);
     $(".education-entry").append(formattedSchoolDates); //Education - Including colleges
+
+    var k=0;
+    while(education.schools[i].majors[k])
+    {
+      var formattedMajorName = HTMLschoolMajor.replace("%data%",education.schools[i].majors[k]);
+      $(".education-entry").append(formattedMajorName);
+      k++;
+    }
+
+    i++;
 }
 
 $(".education-entry").append(HTMLonlineClasses);
 
-for(var i=0; i<2; i++){
+//for(var i=0; i<2; i++){
+var j = 0;
+while(education.schools[j]){
 
-    var formattedOnlineTitle = HTMLonlineTitle.replace("%data%",education.onlineCourses[i].title);
+    var formattedOnlineTitle = HTMLonlineTitle.replace("%data%",education.onlineCourses[j].title);
     $(".education-entry").append(formattedOnlineTitle);
 
-    var formattedOnlineSchool = HTMLonlineSchool.replace("%data%",education.onlineCourses[i].school);
+    var formattedOnlineSchool = HTMLonlineSchool.replace("%data%",education.onlineCourses[j].school);
     $(".education-entry").append(formattedOnlineSchool);
 
-    var formattedOnlineDates = HTMLonlineDates.replace("%data%",education.onlineCourses[i].dates);
+    var formattedOnlineDates = HTMLonlineDates.replace("%data%",education.onlineCourses[j].dates);
     $(".education-entry").append(formattedOnlineDates);
 
-    var formattedOnlineURL = HTMLonlineURL.replace("%data%",education.onlineCourses[i].URL)
+    var formattedOnlineURL = HTMLonlineURL.replace("%data%",education.onlineCourses[j].URL)
     $(".education-entry").append(formattedOnlineURL); //Education - Online Classes
+
+    j++;
 }
 
-
+//Adding Maps to the page.
 $("#mapDiv").append(googleMap);
 
 
